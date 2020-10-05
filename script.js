c501b6510f8f008a52a9ed4db0a63a7ea8b737c4
ciemnicaxdd <57015769+ciemnicaxdd@users.noreply.github.com>
2020-10-05 10:25:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var id = 0;
+var images = document.querySelectorAll('#imgs img');
+var x = images.length;
+
+function rotateoImgs() {
+    imgs.style.transform = `translateX({$id*500}px)`;
+
+    if (x >= id)
+
+
+
+
+}
+setInterval(rotateoImgs, 2000);
